7705f139d84e9ac3db39d9f03a922e9650db341b
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-11-27 16:57:15 +0100

    chore: init project

---
+++ A5/pomodhero/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import ChatElenco from './views/ChatListView.vue';
+    export default {
+        components: { "chat-elenco": ChatElenco },
+        data() {
+            return {}
+        },
+        methods: {
+        }
+    };
+</script>
+
+<template>
+    <div id="app" class="flex flex-col items-center">
+        <chat-elenco></chat-elenco>
+    </div>
+</template>
