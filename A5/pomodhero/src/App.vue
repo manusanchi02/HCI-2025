487417f2daf4c7227835c8e4ab335fa1984c409d
sevetom <sevetom2611@libero.it>
2025-12-02 16:29:24 +0100

    feat: collapsing debug navbar

--- A5/pomodhero/src/App.vue
+++ A5/pomodhero/src/App.vue
@@ -1,11 +1,19 @@
 <script>
-import router from "./router";
-
 export default {
+  data() {
+    return {
+      isNavExpanded: false,
+    };
+  },
   mounted() {
     if (this.$router && this.$router.replace) {
       this.$router.replace("/");
     }
+  },
+  methods: {
+    toggleNav() {
+      this.isNavExpanded = !this.isNavExpanded;
+    },
   },
 };
 </script>
@@ -14,7 +22,13 @@ export default {
   <div id="app" class="flex flex-col items-center w-full">
     <!-- Testing Navbar -->
     <nav class="w-full bg-gray-800 text-white p-4">
-      <div class="flex flex-wrap gap-2 justify-center">
+      <button 
+        @click="toggleNav" 
+        class="w-full mb-1 px-3 py-2 rounded bg-gray-700 hover:bg-gray-600 text-left font-semibold"
+      >
+        {{ isNavExpanded ? '▼' : '▶' }} Debug Navigation
+      </button>
+      <div v-show="isNavExpanded" class="flex flex-wrap gap-2 justify-center">
         <router-link
           to="/"
           class="text-sm sm:text-base px-2 py-1 sm:px-3 sm:py-1.5 rounded bg-gray-700 hover:bg-gray-600"
